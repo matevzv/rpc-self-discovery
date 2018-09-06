150cdfa4c34964450af91ce68f45f81391abc82a
Matevz Vucnik <vucnikm@gmail.com>
2018-09-06 18:13:37 +0200

    python 3 fixes

--- avahi-zerorpc-client.py
+++ avahi-zerorpc-client.py
@@ -3,14 +3,15 @@ import time
 import zerorpc
 import subprocess
 
+service_name = "sna-gw"
 srv = None
 
 while True:
     avahi = subprocess.check_output(["avahi-browse", "-rpt", "_remote._tcp"])
-    reader = csv.reader(avahi.split('\n'), delimiter=';')
+    reader = csv.reader(avahi.decode().split('\n'), delimiter=';')
     for row in reader:
-        if row and row[0] == "=" and "sna-lgtc" in row[6]:
-            print "We have IP: " + row[7]
+        if row and row[0] == "=" and service_name in row[3]:
+            print("We have IP: " + row[7])
             srv = row[7] + ":" + row[8]
     if srv:
         break
@@ -21,5 +22,4 @@ c = zerorpc.Client()
 c.connect("tcp://%s" % srv)
 
 for request in range (10):
-    print c.hello("RPC")
-    time.sleep(1)
+    print(c.hello("RPC"))
